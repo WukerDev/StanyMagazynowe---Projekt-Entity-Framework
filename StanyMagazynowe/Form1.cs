2815304106549c78a06b39b906ad6cb6002362a6
Kodyslaw <122900096+Kodyslaw@users.noreply.github.com>
2023-01-17 18:08:04 +0100

    Update Form1.cs
    added comments clearing up the code

--- StanyMagazynowe/Form1.cs
+++ StanyMagazynowe/Form1.cs
@@ -12,36 +12,36 @@ namespace StanyMagazynowe
 {
     public partial class Form1 : Form
     {
-        int IDSzukane;
+        int IDSzukane;  //tworzymy zmienną do szukania 
         public Form1()
         {
             InitializeComponent();
             
         }
 
-        public void ListBoxLoad()
+        public void ListBoxLoad()  //ładujemy listBoxa
         {
             {
-                listBox1.Items.Clear();
+                listBox1.Items.Clear();  //czyścimy obydwa
                 listBox2.Items.Clear();
-                using (var db = new ObslugaBazy())
+                using (var db = new ObslugaBazy())  //baza danych db o wartości nowej bazy danych stworzonej w pliku OblsugaBazy
                 {
-                    var towary = db.Towar.ToList();
-                    foreach (var towar in towary)
+                    var towary = db.Towar.ToList();  //zamieniamy tabelę towar na listę i przypisujemy do zmiennej towary
+                    foreach (var towar in towary)  //za każdy towar na liście powtarzamy pętlę
                     {
-                        string powlaczone = towar.Nazwa + " " + towar.Jednostka;
-                        listBox1.Items.Add(powlaczone);
+                        string powlaczone = towar.Nazwa + " " + towar.Jednostka;  //string powlaczone (literówka)  ma wartość nazwy towaru, dodajemy spację i wypisujemy jednostkę
+                        listBox1.Items.Add(powlaczone); //dodajemy do listboxa wartość stringa powlaczone
                     }
                     ;
 
                 }
                 using (var db = new ObslugaBazy())
                 {
-                        var ilosci = db.StanMagazynowy.ToList();
-                        foreach (var Ilosc in ilosci)
+                        var ilosci = db.StanMagazynowy.ToList(); //tutaj jak wyżej, robimy zmienną ilosci do której jest przypisana lista stworzona z bazy danych zawierające zmienne z tabeli StanMagazynowy
+                        foreach (var Ilosc in ilosci) // za każdą ilość w tabeli zmiennej ilosci pętla powtarza się
                         {
 
-                            listBox2.Items.Add(Ilosc.Ilosc);
+                            listBox2.Items.Add(Ilosc.Ilosc); //do listboxa dodajemy kolejne przedmioty 
 
                         }
 
@@ -50,22 +50,22 @@ namespace StanyMagazynowe
 
             }
         }
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)  //dodajemy dane do bazy
         {
-            ListBoxLoad();
-            string[] wartosci = { textBox1.Text, textBox2.Text, textBox3.Text };
-            ObslugaBazy baza = new ObslugaBazy();
-            baza.Towar.Add(new Towar { Nazwa = wartosci[0], Jednostka = wartosci[1] });
-            baza.SaveChanges();
-            IDSzukane = baza.Towar.Count();
-            baza.StanMagazynowy.Add(new StanMagazynowy { Ilosc = Convert.ToInt32(wartosci[2]), FKID_Towar = IDSzukane });
-            baza.SaveChanges();
-            ListBoxLoad();
+            ListBoxLoad();  //ładujemy listboxa
+            string[] wartosci = { textBox1.Text, textBox2.Text, textBox3.Text }; //tworzymy tabelę o nazwie wartośći która będzie posiadała wartości z 3 textboxów
+            ObslugaBazy baza = new ObslugaBazy(); //tworzymy obiekt baza by korzystać z metod w klasie ObslugaBazy
+            baza.Towar.Add(new Towar { Nazwa = wartosci[0], Jednostka = wartosci[1] }); //używając obiektu baza wywołujemy funkcję dodawania do tabeli Towar, którego wartoscią będzie pierwsza wartosc w tabeli Wartosci i ustalamy ze zmienna Jednostki jest równa drugiej pozycji w tabeli wartosci 
+            baza.SaveChanges();  //zapisujemy zmiany
+            IDSzukane = baza.Towar.Count(); //ustalamy że zmienna IDSzukane ma wartość ilości zmiennych w tabeli Towar
+            baza.StanMagazynowy.Add(new StanMagazynowy { Ilosc = Convert.ToInt32(wartosci[2]), FKID_Towar = IDSzukane }); //dodajemy nowy stan magazynowy, jak wyżej z towarem
+            baza.SaveChanges(); // zapisujemy zmiany
+            ListBoxLoad(); //ładujemy listBoxa
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)  //drugi przycisk ładuje nam tylko dane
         {
-            ListBoxLoad();
+            ListBoxLoad();  //wywołujemy metodę
         }
     }
 }
